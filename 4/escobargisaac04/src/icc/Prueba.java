6267116d2ded25c4fc2f5b8b684dd68114406cd8
IsaacEsc09 <145156943+IsaacEsc09@users.noreply.github.com>
2023-10-09 00:51:40 -0600

    Update Prueba.java

--- 4/escobargisaac04/src/icc/Prueba.java
+++ 4/escobargisaac04/src/icc/Prueba.java
@@ -1,4 +1,3 @@
-
 package icc;
 
 import java.util.Scanner;
@@ -53,7 +52,66 @@ public class Prueba {
     public static void main(String args[]) {
         int opcion;
         String mensaje, error;
-        StringBuilder sb;
-    }
+        Elipse e = null; // Declaración de la variable e fuera del bucle
 
+        Scanner entrada = new Scanner(System.in);
+
+        System.out.println("Este programa realiza algunas operaciones con la elipse.");
+        System.out.println("1. Obtener el perimetro de una elipse.");
+        System.out.println("2. Obtener el area de una elipse.");
+        System.out.println("3. Revisar si un circulo se encuentra en el borde de una elipse.");
+        System.out.println("0. Salir.");
+        System.out.println("Escoje una opcion.");
+        opcion = getInt("Escoje una opcion.", "Opción no válida. Introduce un número entre 0 y 3.", 0, 3);
+
+        do {
+            switch (opcion) {
+                case 1: {
+                    double mayor = getDouble("Ingresa el eje mayor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+                    double menor = getDouble("Ingresa el eje menor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+
+                    e = new Elipse(mayor, menor);
+                    System.out.println("El perímetro de la elipse es: " + e.perimetro());
+                    break;
+                }
+                case 2: {
+                    double mayor = getDouble("Ingresa el eje mayor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+                    double menor = getDouble("Ingresa el eje menor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+
+                    e = new Elipse(mayor, menor);
+                    System.out.println("El área de la elipse es: " + e.area());
+                    break;
+                }
+                case 3: {
+                double mayor = getDouble("Ingresa el eje mayor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+                double menor = getDouble("Ingresa el eje menor del elipse.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+
+                e = new Elipse(mayor, menor);
+
+                double radioCirculo = getDouble("Ingresa el radio del círculo.", "Valor no válido. Introduce un número mayor que 0.", 0.0, Double.MAX_VALUE);
+                Circulo circulo = new Circulo(radioCirculo);
+
+                if (e.encaja(circulo)) {
+                System.out.println("El círculo encaja con el de la elipse.");
+                } else {
+                System.out.println("El círculo no encaja con el de la elipse.");
+                }
+                break;
+                }
+                case 0: {
+                    System.out.println("Vuelve pronto :)");
+                    break;
+                }
+                default: {
+                    System.out.println("Opción no válida. Introduce un número entre 0 y 3.");
+                    break;
+                }
+            }
+
+            if (opcion != 0) {
+                opcion = getInt("Escoje una opcion.", "Opción no válida. Introduce un número entre 0 y 3.", 0, 3);
+            }
+
+        } while (opcion != 0);
+    }
 }
